426ba8bfb5c10a8462ac864cc2eb244d80617a7c
kengirie <kengoi.6330@gmail.com>
2025-04-20 13:24:42 +0900

    Fix translation texts and update relay management page styles

--- src/pages/relays/index.tsx
+++ src/pages/relays/index.tsx
@@ -2,7 +2,7 @@ import { RelaysWidget } from '@/features/relays-widget';
 
 export const RelaysPage = () => {
   return (
-    <div className="space-y-6">
+    <div className="space-y-8 p-4">
       <RelaysWidget />
     </div>
   );
